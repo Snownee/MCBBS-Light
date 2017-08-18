e290736ff74151447b47f146cf4d7e5a065b4268
Snownee <1850986885@qq.com>
2017-08-18 10:04:31 +0800

    0.2

--- mcbbs-light.js
+++ mcbbs-light.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mcbbs-Light
 // @namespace    undefined
-// @version      0.1.1
+// @version      0.2
 // @description  the new
 // @author       Snownee
 // @include      *mcbbs.net/*
